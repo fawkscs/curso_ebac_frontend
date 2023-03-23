9743779a84769937e8db1ac7f1f0e8f13d2a792a
Bruno Gonçalves <brun@yampi.com.br>
2023-03-23 04:44:53 -0300

    commit inicial

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-const form = document.getElementById("meuForm");
-      const mensagem = document.getElementById("mensagem");
-
-      form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Impede o envio do formulário
-
-        const campoA = Number(document.getElementById("campoA").value);
-        const campoB = Number(document.getElementById("campoB").value);
-
-        if (campoB > campoA) {
-          mensagem.innerHTML = 'Formulário válido!';
-          mensagem.classList.remove("mensagem-invalida");
-          mensagem.classList.add("mensagem-valida");
-        } else {
-          mensagem.innerHTML = 'Formulário inválido: campo B deve ser maior que campo A.';
-          mensagem.classList.remove("mensagem-valida");
-          mensagem.classList.add("mensagem-invalida");
-        }
-      });
+$(document).ready(function() {
+	// Adicionar tarefa
+	$('#add-task').on('click', function(e) {
+		e.preventDefault(); // Evita que a página seja recarregada ao clicar no botão
+		
+		// Pega o valor do campo de texto
+		var task = $('#task-input').val();
+		
+		// Adiciona a tarefa na lista
+		$('#task-list').append('<li>' + task + '</li>');
+		
+		// Limpa o campo de texto
+		$('#task-input').val('');
+	});
+	
+	// Marcar tarefa como concluída
+	$('#task-list').on('click', 'li', function() {
+		$(this).toggleClass('completed');
+	});
+});
