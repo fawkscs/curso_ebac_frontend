b2773bc582b155965893c908bafb4c5be32e1bf5
Bruno Gonçalves <brun@yampi.com.br>
2023-03-23 05:55:18 -0300

    commit projeto finalizado

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-$(document).ready(function() {
-	// Adicionar tarefa
-	$('#add-task').on('click', function(e) {
-		e.preventDefault(); // Evita que a página seja recarregada ao clicar no botão
-		
-		// Pega o valor do campo de texto
-		var task = $('#task-input').val();
-		
-		// Adiciona a tarefa na lista
-		$('#task-list').append('<li>' + task + '</li>');
-		
-		// Limpa o campo de texto
-		$('#task-input').val('');
-	});
-	
-	// Marcar tarefa como concluída
-	$('#task-list').on('click', 'li', function() {
-		$(this).toggleClass('completed');
-	});
+$(document).ready(function () {
+	// Adicione a máscara para o campo de telefone
+	$('#telefone').mask('(00) 00000-0000');
+
+	// Adicione a máscara para o campo de CPF
+	$('#cpf').mask('000.000.000-00');
+
+	// Adicione a máscara para o campo de CEP
+	$('#cep').mask('00000-000');
 });
+
+function mostrarDados() {
+	const nome = document.getElementById("nome").value;
+	const email = document.getElementById("email").value;
+	const telefone = document.getElementById("telefone").value;
+	const cpf = document.getElementById("cpf").value;
+	const endereco = document.getElementById("endereco").value;
+	const cep = document.getElementById("cep").value;
+	alert("Nome: " + nome + "\nE-mail: " + email + "\n" +
+		"Telefone" + telefone + "\nCPF" + cpf + "\n" +
+		"Endereço: " + endereco + "\nCEP: " + cep);
+}
